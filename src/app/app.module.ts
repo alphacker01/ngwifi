a4b81b6a5678778838f441bb36ca9cb74c439bbd
Alphacker <alphaoumar3940@gmail.com>
2020-08-30 19:29:44 +0000

    My first component in Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlphaComponent } from './alpha/alpha.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlphaComponent
   ],
   imports: [
     BrowserModule,
